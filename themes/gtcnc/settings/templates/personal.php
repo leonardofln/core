d32779c959c6436776c8883c451d25a77d5360c9
leonardofln <leonardofln@gmail.com>
2014-05-20 21:37:59 -0300

    algumas informações retiradas da página pessoal do usuário (cota, versão e etc)

--- themes/gtcnc/settings/templates/personal.php
+++ themes/gtcnc/settings/templates/personal.php
@@ -4,15 +4,6 @@
  * See the COPYING-README file.
  */?>
 
-<div id="quota" class="personalblock">
-	<div style="width:<?php p($_['usage_relative']);?>%;">
-		<p id="quotatext">
-			<?php print_unescaped($l->t('You have used <strong>%s</strong> of the available <strong>%s</strong>',
-			array($_['usage'], $_['total_space'])));?>
-		</p>
-	</div>
-</div>
-
 <?php
 if($_['displayNameChangeSupported']) {
 ?>
@@ -60,16 +51,3 @@ if($_['passwordChangeSupported']) {
 		</select>
 	</fieldset>
 </form>
-
-<fieldset class="personalblock">
-	<h2><?php p($l->t('Version'));?></h2>
-	<strong><?php p($theme->getName()); ?></strong> <?php p(OC_Util::getHumanVersion()) ?><br />
-<?php if (OC_Util::getEditionString() === ''): ?>
-	<?php print_unescaped($l->t('Developed by the <a href="http://ownCloud.org/contact" target="_blank">ownCloud community</a>, the <a href="https://github.com/owncloud" target="_blank">source code</a> is licensed under the <a href="http://www.gnu.org/licenses/agpl-3.0.html" target="_blank"><abbr title="Affero General Public License">AGPL</abbr></a>.')); ?>
-<?php endif; ?>
-</fieldset>
-<fieldset class="personalblock credits-footer">
-<p>
-	<?php print_unescaped($theme->getShortFooter()); ?>
-</p>
-</fieldset>

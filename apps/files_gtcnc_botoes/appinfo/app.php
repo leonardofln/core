1efc40981736f3e3618e8f5b39918f3937a8ce3a
leonardofln <leonardofln@gmail.com>
2014-07-10 22:27:47 -0300

    removendo arquivos da versão antiga

--- apps/files_gtcnc_botoes/appinfo/app.php
+++
@@ -1,4 +0,0 @@
-<?php
-if(OC::$REQUESTEDAPP == 'files') {
-	OCP\Util::addScript('files_gtcnc_botoes', 'gtcnc_botoes');
-}
--- apps/files_gtcnc_botoes/js/gtcnc_botoes.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function() {
-	$("div[id='controls']").bind("DOMSubtreeModified",function(){
-		if ($("div[class^='crumb']").length == 1) {
-			$("div[class^='actions creatable']").hide();
-		} else {
-			$("div[class^='actions creatable']").show();
-		}
-	});
-});
